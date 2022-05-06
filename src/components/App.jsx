55b0997afaa158bced8814035ffa4e88c4a3c342
Stas Sytnykov <stas82kak05@gmail.com>
2022-05-06 21:58:05 +0300

    add default markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  onChangeInput = event => {
+    console.log(event.target.name);
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form action="">
+          <h2>Phonebook</h2>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.onChangeInput}
+            />
+          </label>
+
+          <button type="submit">Add Contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul></ul>
+      </div>
+    );
+  }
+}
